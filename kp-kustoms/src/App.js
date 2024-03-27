c622d92830f245af3d77d090f835b99c78724f6e
Krystyna Poremba <poremba.krystyna@gmail.com>
2024-03-27 15:16:20 -0400

    Begin to implement square checkout process

--- kp-kustoms/src/App.js
+++ kp-kustoms/src/App.js
@@ -14,6 +14,7 @@ import Shop from "./Pages/Shop/Shop";
 import Product from "./Pages/Product/Product";
 import Wishlist from "./Pages/Wishlist/Wishlist";
 import Cart from "./Pages/Cart/Cart";
+import Checkout from "./Pages/Checkout/Checkout";
 
 function KpKustoms() {
   return (
@@ -30,6 +31,7 @@ function KpKustoms() {
           <Route path="/FAQ" element={<FAQ />}></Route>
           <Route path="/Wishlist" element={<Wishlist />}></Route>
           <Route path="/Cart" element={<Cart />}></Route>
+          <Route path="/Checkout" element={<Checkout />}></Route>
           <Route path="/*" element={<Home />} />
         </Routes>
         <Footer />
